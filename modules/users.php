ec7ffa1dd0579ea35a80ab8e583e15a9537ac272
java <ladbrookm@gmail.com>
2018-04-26 08:46:36 +1200

    Moved past location function
    Past location function was moved from user module to the location module as this makes more sense.

--- modules/users.php
+++ modules/users.php
@@ -14,17 +14,8 @@ function get_user($user_id) {
   }
 }
 
-function get_user_past_location($user_id) {
   if (isset($conn)) {
-    $location_query = "SELECT movements.move_id, x.loc_name, y.loc_name, people.fname, people.lname, movements.move_time ";
-    $location_query .= "FROM movements ";
-    $location_query .= "LEFT JOIN locations x ON movements.move_from = x.loc_id ";
-    $location_query .= "LEFT JOIN locations y ON y.loc_id = movements.move_to ";
-    $location_query .= "JOIN people ON people.user_id = movements.move_user ";
-    $location_query .= "WHERE move_user = " . $user_id;
-    $location_result = mysqli_query($conn, $location_query);
   } else {
-    
   }
 }
 ?>
